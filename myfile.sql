8905b554cafe6a15a02f7eb471b704c75a971eba
kasturez <tinhuynhvinh733@gmail.com>
2020-12-17 09:50:16 +0700

    Update myfile.sql

--- myfile.sql
+++ myfile.sql
@@ -9,8 +9,9 @@ DROP TABLE IF EXISTS inventory;
 
 CREATE TABLE doctor
 (
-    ID        VARCHAR(3) ,
-    name      VARCHAR(50),
+    ID                      VARCHAR(3),
+    name                    VARCHAR(50),
+    gender                  ENUM ('male', 'female'),
     `startDate(yyyy-mm-dd)` DATE,
     CONSTRAINT pk_doctor PRIMARY KEY (ID)
 );
@@ -41,16 +42,16 @@ CREATE TABLE patient
 
 CREATE TABLE room
 (
-    number    VARCHAR(3),
-    slot      INTEGER,
-    disease   VARCHAR(3),
+    number  VARCHAR(3),
+    slot    INTEGER,
+    disease VARCHAR(3),
     CONSTRAINT pk_room PRIMARY KEY (number)
 );
 
 CREATE TABLE inventory
 (
-    ID VARCHAR(3),
-    name VARCHAR(50),
+    ID    VARCHAR(3),
+    name  VARCHAR(50),
     stock INT UNSIGNED,
 
     CONSTRAINT pk_inventory PRIMARY KEY (ID)
@@ -59,7 +60,7 @@ CREATE TABLE inventory
 CREATE TABLE `case`
 (
     case_id        INT UNSIGNED,
-    doctor_id      VARCHAR(3)   ,
+    doctor_id      VARCHAR(3),
     patient_id     INT UNSIGNED,
     room_number    VARCHAR(3),
     emergency_case BOOLEAN,
@@ -67,17 +68,17 @@ CREATE TABLE `case`
     operation_time TIME,
     disease_id     VARCHAR(3),
     treatment_id   VARCHAR(3),
-    resource_id VARCHAR(3),
+    resource_id    VARCHAR(3),
 
-    CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES doctor (ID) ON UPDATE CASCADE ON DELETE CASCADE ,
-    CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patient (ID) ON UPDATE CASCADE ON DELETE CASCADE ,
-    CONSTRAINT fk_room_number FOREIGN KEY (room_number) REFERENCES room (number) ON UPDATE CASCADE ON DELETE CASCADE ,
-    CONSTRAINT fk_disease_id FOREIGN KEY (disease_id) REFERENCES disease (ID) ON UPDATE CASCADE ON DELETE CASCADE ,
-    CONSTRAINT fk_treatment_id FOREIGN KEY (treatment_id) REFERENCES treatment (ID) ON UPDATE CASCADE ON DELETE CASCADE ,
+    CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES doctor (ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patient (ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_room_number FOREIGN KEY (room_number) REFERENCES room (number) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_disease_id FOREIGN KEY (disease_id) REFERENCES disease (ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_treatment_id FOREIGN KEY (treatment_id) REFERENCES treatment (ID) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT fk_resource_id FOREIGN KEY (resource_id) REFERENCES inventory (ID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-INSERT INTO doctor(id, name, `startdate(yyyy-mm-dd)`)
-VALUES ('001', 'John', '2020-12-21')
+INSERT INTO doctor(id, name, gender, `startdate(yyyy-mm-dd)`)
+VALUES ('001', 'John', 'male', '2020-12-21')
 
 
